40ae12f1ed18c81c05aa442249198c30cabf58cb
Martin Kovaľ <martinkoval28@gmail.com>
2026-04-08 09:37:44 +0200

    hotove

--- searching.py
+++ searching.py
@@ -3,13 +3,20 @@ import json
 
 
 def read_data(file_name, field):
-    with open(file_name, "r") as fil:
-        dat = json.load(fil)
+    #get current working directory path
+    cwd_path = Path.cwd()
 
-        if field not in dat.keys():
-            return None
-        else:
-            return field[dat]
+    file_path = cwd_path / file_name
+
+
+    if field not in {"unordered_numbers", "sequential.json", "dna_sequence"}:
+        return None
+    else:
+        with open(file_path, "r") as fil:
+            dat = json.load(fil)
+        return dat[field]
+
+
     """
     Reads a JSON file and returns data for a given field.
 
@@ -25,9 +32,20 @@ def read_data(file_name, field):
             - None: If the field is not supported.
     """
     # get current working directory path
-    cwd_path = Path.cwd()
-    
-    file_path = cwd_path / file_name
+
+def linear_search(sequence, number):
+    count = 0
+    pozicia = []
+
+    for index, numb in enumerate(sequence):
+        if number == numb:
+            count += 1
+        pozicia.append(index)
+    dict = {"position": pozicia, "count":count}
+    return dict
+
+
+
 
 
 def main():
